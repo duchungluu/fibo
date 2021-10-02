b8385d88ff23f42e7263c17723a12ae59f9080e2
Hung Luu <duchung.luu@live.com>
2021-10-02 13:34:51 +0300

    Add more components

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const redis = require('redis');
 const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
-    retry_strategy: () => 1000;
+    retry_strategy: () => 1000
 });
 
 const redisPublisher = redisClient.duplicate();
@@ -45,4 +45,31 @@ app.get('/', (req,res) => {
 
 app.get('/values/all', async (req,res) => {
     const values = await pgClient.query('SELECT * from values');
+
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req,res) => {
+    //get all hash value
+    redisClient.hgetall('values', (err,values) => {
+        res.send(values);
+    });
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high');
+    }
+
+    redisClient.hset('values', index, 'Nothing yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Listening');
 })
